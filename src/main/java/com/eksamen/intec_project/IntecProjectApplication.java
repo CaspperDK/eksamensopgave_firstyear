56331e43aab0dd74b51527a6d192c38a8a0cb491
CasperW <westergaard2002@gmail.com>
2023-05-08 17:11:25 +0200

    Upload af hele projektet.

---
+++ src/main/java/com/eksamen/intec_project/IntecProjectApplication.java
@@ -0,0 +1,13 @@
+package com.eksamen.intec_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntecProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntecProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eksamen/intec_project/IntecProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eksamen.intec_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntecProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
